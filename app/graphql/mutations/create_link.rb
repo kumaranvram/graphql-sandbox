d9b588b09ccbd6b528e8b0cc707b1d4e218abc3e
Kumaran V <kumaran.v.ram@gmail.com>
2019-12-09 16:36:18 -0500

    Add mutations to create new links

---
+++ app/graphql/mutations/create_link.rb
@@ -0,0 +1,15 @@
+module Mutations
+    class CreateLink < Mutations::BaseMutation
+        argument :url, String, required: true
+        argument :description, String, required: true
+    
+        type Types::LinkType
+        
+        def resolve(url: nil, description: nil)
+            Link.create!(
+                url: url,
+                description: description,
+            )
+        end
+    end
+end
